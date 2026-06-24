933144d56fc4670472974a7ec3349334bb96d5e6
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-06-24 12:32:51 +0545

    added css for better syling in newcollection

--- Frontend/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,11 +1,11 @@
 // import React from 'react';
-import "./NewCollections.css"
+import "./NewCollections.css";
 import new_collection from "../assets/new_collections";
 import Item from "../Item/Item";
 
 const NewCollections = () => {
   return (
-    <div classname= "new-collections">
+    <div className= "new-collections">
       <h1>New Collections</h1>
       <hr/>
       <div className="collections">
